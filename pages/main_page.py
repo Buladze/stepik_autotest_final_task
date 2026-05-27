20c8560f133420a4ed5b5932c3d4d6e7ead2db71
Buladze <buladze@example.com>
2026-05-27 12:18:23 +0300

    changed main_page according to locators.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,11 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 class MainPage(BasePage): 
     def go_to_search(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "[class='gLFyf']")
+        login_link = self.browser.find_element(*MainPageLocators.SEARCH_BOX)
         login_link.click()
     
     def should_be_headline(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "[aria-label='Google']"), "Should be 'Google'"
+        assert self.is_element_present(*MainPageLocators.GOOGLE_HEADLINE), "Should be 'Google'"
